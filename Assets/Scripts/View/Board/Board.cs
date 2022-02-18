87615be5b9d7ad54986c0cc7cd8f6c6f0b7cd971
97saundersj <jacques_s@hotmail.co.uk>
2022-02-18 20:51:44 +0000

    Added New AR cursor

--- Assets/Scripts/View/Board/Board.cs
+++ Assets/Scripts/View/Board/Board.cs
@@ -101,28 +101,32 @@ namespace BoardTools
 
             // Enable Portrait mode
             Screen.autorotateToPortrait = true;
-            Screen.autorotateToPortraitUpsideDown = true;
 
             LoadSceneFromResources("ARScene");
 
             Texture playmatTexture = (Texture)Resources.Load("Playmats/Playmat" + Options.Playmat + "Texture", typeof(Texture));
             GameObject.Find("SceneHolder/ARScene/TableClassic/Playmat").GetComponent<Renderer>().material.mainTexture = playmatTexture;
 
-            // Disable Camera Script so that movement only works through AR
-            //GameObject.Find("CameraHolder").GetComponent<CameraScript>().enabled = false;
-
             // Enable AR scripts
-            GameObject.Find("ARManager").transform.Find("AR Session").gameObject.SetActive(true);
-            GameObject.Find("CameraHolder").transform.Find("ARSessionOrigin").gameObject.SetActive(true);
-            GameObject.Find("CameraHolder/ARSessionOrigin").transform.Find("AR Camera").gameObject.SetActive(true);
-            //GameObject.Find("SceneHolder/").AddComponent<ARAnchor>();
+            //GameObject.Find("ARManager").transform.Find("AR Session").gameObject.SetActive(true);
+            //GameObject.Find("CameraHolder").transform.Find("ARSessionOrigin").gameObject.SetActive(true);
+            //GameObject.Find("CameraHolder/ARSessionOrigin").transform.Find("AR Camera").gameObject.SetActive(true);
+            //var objects = GameObject.Find("ARManager").GetComponentInChildren<GameObject>(true);
+            GameObject arManager = GameObject.Find("ARManager");
+            foreach (Transform child in arManager.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
 
-            // Disable Main Camera
+            // Disable Elements
             GameObject.Find("CameraHolder/Main Camera/").SetActive(false);
+            GameObject.Find("SceneHolder").SetActive(false);
 
             // Rotate Camera back to default and scale camera to match world
-            GameObject.Find("CameraHolder").transform.rotation = new Quaternion(0, 0, 0, 0);
-            GameObject.Find("CameraHolder").transform.localScale = new Vector3(AR_WORLD_SCALE, AR_WORLD_SCALE, AR_WORLD_SCALE);
+            Transform arSessionOrigin = GameObject.Find("CameraHolder").transform.Find("ARSessionOrigin");
+            arSessionOrigin.gameObject.SetActive(true);
+            //arSessionOrigin.transform.rotation = new Quaternion(0, 0, 0, 0);
+            arSessionOrigin.localScale = new Vector3(AR_WORLD_SCALE, AR_WORLD_SCALE, AR_WORLD_SCALE);
         }
 
         private static void LoadSceneFromResources(string sceneName)
